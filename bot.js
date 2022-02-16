bbc4000eb09388cdb797cc207da6599b7d5713b6
louis nwamadi <louisdante9@gmail.com>
2022-02-16 02:31:51 +0100

    add start script

--- bot.js
+++ bot.js
@@ -1,31 +1,36 @@
-const express = require('express')
-const { json, urlencoded } = require('body-parser')
-const app = express();
-const PORT = process.env.PORT || 3000
-app.use(json());
-app.use(urlencoded({ extended: false }));
+const Express = require('express')
+const bodyParser = require('body-parser')
 
-app.post('/bot', (req, res)=> {
-    console.log(res.send, 'request was successful')
+const app = new Express()
+app.use(bodyParser.urlencoded({extended: true}))
+
+const port = process.env.PORT || 80
+
+app.post('/bot', (req, res) => {
+ res.send({
+    "blocks": [
+		{
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": "*It's 80 degrees right now.*"
+			}
+		},
+		{
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": "Partly cloudy today and tomorrow"
+			}
+		}
+	]
+}
+)
+});
+app.get('*', (req, res) => {
+ console.log("This route doesn\'t exist")
+});
+
+app.listen(port, () => {
+  console.log(`Server started at localhost:${port}`)
 })
-
-app.listen(PORT, ()=> {
-    console.log(`server is running on port ${PORT}`)
-})
-
-// var http = require('http');
-// const port = process.env.PORT || 3000
-// http.createServer(function (req, res) {
-    
-//    if (req.url === "/") {
-//       res.writeHead(200, {'Content-Type': 'text/plain'});
-//     res.end('Hello World\n');
-//    } else if (req.url === "/bot" && req.method === "POST") {
-//       res.write("This is about page.");
-//       res.end();
-//    } else {
-//       res.write("Not Found!");
-//       res.end();
-//    }
-// }).listen(port, "0.0.0.0");
-// console.log('Server running at port ' + port);
